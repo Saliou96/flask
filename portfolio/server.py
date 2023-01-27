79e0202c63282cb78bafe94f25d790216deb7bc5
Serigne Saliou Mbacké NDIAYE <serignesalioumbackendiaye@esp.sn>
2023-01-27 10:00:00 +0000

    aa

--- portfolio/server.py
+++ portfolio/server.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, request, redirect
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def home():
+    return render_template('index.html')
